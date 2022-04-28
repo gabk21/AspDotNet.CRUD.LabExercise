76018ab3be75a2a89d199ef160a891dbdf9c25b3
[gabriel.kho] <[gabriel.kho@pointwest.com.ph]>
2022-04-28 22:21:53 +0800

    first commit

---
+++ CustomerData/Context/CustomerDbContext.cs
@@ -0,0 +1,34 @@
+﻿using CustomerData.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerData.Context
+{
+    public class CustomerDbContext: DbContext
+    {
+        private readonly string connectionString;
+
+        public CustomerDbContext(): this("Server=7LMBQG3;Database=CustomerDb;User Id=sa;Password=invoker21")
+        {
+
+        }
+
+        public CustomerDbContext(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(this.connectionString);
+            }
+        }
+    }
+}
---
+++ CustomerData/Repositories/CustomerRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomerData.Context;
+using CustomerData.Models;
+
+namespace CustomerData.Repositories
+{
+    public interface ICustomerRepository : IBaseRepository<Customer>
+    {
+    }
+
+    public class CustomerRepository : GenericRepository<Customer>, ICustomerRepository
+    {
+        public CustomerRepository(CustomerDbContext context) : base(context)
+        {
+        }
+    }
+}
